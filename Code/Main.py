2888323e1351c9da20c96681cef4bd70ce46f5ff
Bayleylovett <92125254+Bayleylovett@users.noreply.github.com>
2022-06-05 14:24:07 +1000

    Starting to make app

--- Code/Main.py
+++ Code/Main.py
@@ -1,6 +1,33 @@
+from User import userSwitch
+from Admin import adminSwitch
 
+global type
+
+
+
+def login():
+    userID = input('\nEnter your user identification number:')
+    password = input('\nEnter your password:')
+
+    #insert select satement and then get type
+
+
+
+#Switch statement based on user permissions
+def switch():
+    while type != '0':
+        if type == '0':
+               print('\nGood Bye')
+        elif type == 'user':
+                userSwitch()
+        elif type == 'admin':
+            adminSwitch()
+
+    if type == '0':
+        print('\nGood Bye')
 
 if __name__ == '__main__':
-    print("Hi")
+    login()
+    switch()
 
 
